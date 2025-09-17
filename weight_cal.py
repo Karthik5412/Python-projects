3ed87b1ca8d6d2f2a97714a4ec8d445522b63407
Karthik5412 <kkuricharlapati@gmail.com>
2025-09-17 21:15:44 +0530

    commit

--- weight_cal.py
+++ weight_cal.py
@@ -1,1 +1,14 @@
-print('I am Karthik from India')
+weight = input('Enter your weight along with units: ')
+
+if 'kg' in weight :
+    we = weight.replace('kg'or' kg','')
+    w = int(we) * 2.205
+    print(f'{w} lbs is your weight in pounds')
+
+elif 'lbs' in weight :
+    we = weight.replace('lbs'or' lbs','')
+    w = int(we) / 2.205
+    print(f'{round(w,3)} kg is your weight in kilograms')
+else :
+    print(f'{weight} is not allowed')
+
